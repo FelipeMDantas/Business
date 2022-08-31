b175cc9b64e6eb8623bc7fcdcf45218e3edd14d5
FelipeMDantas <felipemdantas@terra.com.br>
2022-08-30 23:56:53 -0300

    Buttons and Cards stylization.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,4 +1,26 @@
 import styles, { layout } from "../style";
+import Button from "../components/Button";
+import { features } from "../constants";
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p>{content}</p>
+    </div>
+  </div>
+);
 
 const Business = () => (
   <section id="features" className={layout.section}>
@@ -12,6 +34,14 @@ const Business = () => (
         building credit, earning rewards and saving money. But with hundred of
         credit cards on the market.
       </p>
+
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
     </div>
   </section>
 );
