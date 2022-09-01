e423609306df945a11c56d6483dee1747fe27401
FelipeMDantas <felipemdantas@terra.com.br>
2022-08-31 23:58:20 -0300

    Billing section coded.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -17,7 +17,9 @@ const FeatureCard = ({ icon, title, content, index }) => (
       <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
         {title}
       </h4>
-      <p>{content}</p>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
     </div>
   </div>
 );
